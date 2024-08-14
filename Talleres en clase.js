fc32d74a6b294ff969b4a90b6f5b1965153085bf
Venus1an <150965181+Venus1an@users.noreply.github.com>
2024-08-14 09:39:02 -0500

    Talleres en clase.js

---
+++ Talleres en clase.js
@@ -0,0 +1,7 @@
+let puntaje = "65"; //Agosto 13
+    if(puntaje>=90){
+console.log("Excelente");
+}else if(puntaje>=70 && puntaje<=89){
+console.log("Normal");
+}else (puntaje<70)
+console.log("Nesecita mejorar");
